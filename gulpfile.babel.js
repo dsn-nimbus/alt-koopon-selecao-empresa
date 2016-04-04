c1e1a2aca70863828e2d39a930c3da738df52055
Phillip Miguel dos Passos <phillipmigueldospassos@gmail.com>
2016-04-04 12:59:42 -0300

    Correcao do gulp. Alterado navegador para o PhantomJS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -35,7 +35,7 @@ gulp.task('unit_test', (done) => {
   let _opts = {
     configFile: __dirname + '/karma.conf.js',
     singleRun: true,
-    browsers: ['Chrome']
+    browsers: ['PhantomJS']
   };
 
   return karma.start(_opts, done);
